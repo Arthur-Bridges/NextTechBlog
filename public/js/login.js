ec025b785edb9f23c8428521ba2ea721325e7581
Arthur Bridges <arthurbridges08@yahoo.com>
2023-11-30 23:45:11 -0600

    tying up the front end login

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,42 @@
 //factory function setup
-async function loginH(event) {
-    let username = document.getElementById()
+async function (event) {
+  event.preventDefault();
+
+  // Getting user input
+  let username = document.getElementById("user-name").value.trim();
+  let password = document.getElementById("password").value.trim();
+
+  // Validate username
+  if (!username) {
+    let usernameField = document.getElementById("user-name");
+    usernameField.style.backgroundColor = "rgb(255, 192, 203)";
+    usernameField.style.borderColor = "pink";
+    usernameField.placeholder = "Please enter a username";
+  }
+
+  // Validate password
+  if (!password) {
+    let passwordField = document.getElementById("password");
+    passwordField.style.backgroundColor = "rgb(255, 192, 203)";
+    passwordField.style.borderColor = "pink";
+    passwordField.placeholder = "Please enter a password";
+  }
+
+  // Validation after UserName and PW are verified
+  if (username && password) {
+    const response = await fetch("/api/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await response.json();
+    if (response.status === 400 || response.status === 401) {
+      alert(data.message);
+    } else {
+      window.location.replace("/");
+    }
+  }
 }
+
+// Event listener for the login button
+document.getElementById("loginBtn").addEventListener("click", loginHandler);
